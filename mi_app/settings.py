1f95a107f62f57b46598eddb9e69dde8093778a0
jossmirr <92958956+jossmirr@users.noreply.github.com>
2025-07-08 09:05:46 -0500

    Update settings.py

--- mi_app/settings.py
+++ mi_app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
